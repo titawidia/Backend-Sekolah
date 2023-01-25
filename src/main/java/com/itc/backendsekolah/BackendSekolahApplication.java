bd38ae12b1b064f162843ae302757a1cc76ed3ee
titawidia <gitawidia166@gmail.com>
2023-01-25 08:50:51 +0700

    First Installing

---
+++ src/main/java/com/itc/backendsekolah/BackendSekolahApplication.java
@@ -0,0 +1,13 @@
+package com.itc.backendsekolah;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSekolahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSekolahApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itc/backendsekolah/BackendSekolahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.backendsekolah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSekolahApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
