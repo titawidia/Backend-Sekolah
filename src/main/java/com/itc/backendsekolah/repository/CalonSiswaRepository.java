bf764dc1115d37b6f4be73d77797b3699a381462
titawidia <gitawidia166@gmail.com>
2023-01-25 09:25:19 +0700

    Calon Siswa Repository

---
+++ src/main/java/com/itc/backendsekolah/repository/CalonSiswaRepository.java
@@ -0,0 +1,7 @@
+package com.itc.backendsekolah.repository;
+
+import com.itc.backendsekolah.entity.CalonSiswa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CalonSiswaRepository extends JpaRepository<CalonSiswa, Long> {
+}
